956453633a4c525c1916ea2b4377f1c578be968b
Gabriel442018 <37821374+Gabriel4420@users.noreply.github.com>
2025-06-02 15:09:00 -0300

    feat: :sparkles: adding header component with navigation and login and shopping cart buttons.

---
+++ src/components/layout/header.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
+export const Header = () => {
+  return (
+    <header className="container mx-auto my-4 flex items-center justify-between p-5 bg-secondary rounded-md">
+      <Link href="/">
+        <h2 className="text-2xl font-bold">B7Pizzas</h2>
+      </Link>
+      <div className="flex gap-2">
+        <Button>Login/Cadastro</Button>
+
+        <Button className="relative">
+          <span className="absolute -top-1 -right-1 flex items-center justify-center w-4 h-4 text-xs font-bold text-white bg-red-500 rounded-full">
+            2
+          </span>
+          Carrinho
+        </Button>
+      </div>
+    </header>
+  );
+};
